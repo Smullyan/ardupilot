6d53e9e9a4696c3faced1758e2013a72bd9df950
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2017-06-09 14:20:58 +0700

    AP_Arduino: changed fuel object to AP_Arduino in library parameters

--- libraries/AP_Arduino/AP_Arduino_I2C.cpp
+++ libraries/AP_Arduino/AP_Arduino_I2C.cpp
@@ -6,7 +6,7 @@ const AP_Param::GroupInfo AP_Arduino::var_info[] = {
     // @Description: The new library parameter description goes here
     // @Range: -32768 32767
     // @User: Advanced
-    AP_GROUPINFO("MY_NEW_P", 0, Fuel, _my_new_lib_parameter, MY_NEW_PARAM_DEFAULT),
+    AP_GROUPINFO("MY_NEW_P", 0, AP_Arduino, _my_new_lib_parameter, MY_NEW_PARAM_DEFAULT),
 
     AP_GROUPDEND
 };
