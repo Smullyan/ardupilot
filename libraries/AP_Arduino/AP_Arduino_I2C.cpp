ccab78e73e90087793b01ad9757b969200eae2e8
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2017-06-09 14:27:45 +0700

    AP_Arduino: Fixed typo

--- libraries/AP_Arduino/AP_Arduino_I2C.cpp
+++ libraries/AP_Arduino/AP_Arduino_I2C.cpp
@@ -8,5 +8,5 @@ const AP_Param::GroupInfo AP_Arduino::var_info[] = {
     // @User: Advanced
     AP_GROUPINFO("MY_NEW_P", 0, AP_Arduino, _my_new_lib_parameter, MY_NEW_PARAM_DEFAULT),
 
-    AP_GROUPDEND
+    AP_GROUPEND
 };
