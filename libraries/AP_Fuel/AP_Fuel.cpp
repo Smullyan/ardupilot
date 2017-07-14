d4123b496c351316c05106d680d5c56118a8fb10
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2017-07-14 15:10:12 +0700

    Base functions added to AP_Fuel

---
+++ libraries/AP_Fuel/AP_Fuel.cpp
@@ -0,0 +1,41 @@
+#include <AP_HAL/AP_HAL.h>
+
+#define ARDUINO_I2C_ADDR 42  //0x2A
+
+#define ARDUINO_I2C_BUS 1
+
+
+// probe and initialise the sensor
+bool AP_Fuel::init()
+{
+    //NOTE we use the exact same template from AP_Airspeed_I2C
+    //TODO test the init and read code. Forget everything else.
+
+    _dev = hal.i2c_mgr->get_device(ARDUINO_I2C_BUS, ARDUINO_I2C_ADDR);
+
+    // take i2c bus semaphore
+    if (!_dev || !_dev->get_semaphore()->take(200)) {
+	return false;
+    }
+
+    
+}
+
+void get_data()
+{
+    //NOTE created simple i2c transfer which will store data in
+    //     variable resistor.
+    //TODO find a way to test the read() function.
+    
+    uint8_t data[2];
+
+    _measurement_started_ms = 0;
+
+    if (!_dev->transfer(nullptr, 0, data, sizeof(data))) {
+	return;
+    }
+
+    int16_t resistor;
+    resistor = (data[0] << 8) + data[1];
+    
+}
---
+++ libraries/AP_Fuel/AP_Fuel.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class AP_Fuel
+{
+public:
+
+    /// Constructor
+    AP_Fuel();
+
+    // TODO: once you've finished creating the basic
+    // frontend, create the backend driver
+
+    // Initialise
+    void init(void);
+
+    // Read the data sent from the arduino
+    // TODO might want to consider adding additional parameters
+    // where we extend the arduino send routine by filling the twi
+    // data register and sending the data with multiple variables.
+    void read();
+
+    
+    // return the current data
+     get_data(void) const {
+	return _data;
+    }
+
+private:
+    int16_t          _current_data;
+
+}
