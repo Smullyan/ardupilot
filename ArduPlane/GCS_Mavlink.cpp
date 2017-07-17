f6a656f6cb46707f8347db3fba228871ecd8011e
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2017-07-17 14:47:46 +0700

    ArduPlane: added read_fuel to scheduler and changed battery message

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -289,7 +289,7 @@ void Plane::send_extended_status1(mavlink_channel_t chan)
     int8_t battery_remaining = -1;
 
     if (battery.has_current() && battery.healthy()) {
-        battery_remaining = battery.capacity_remaining_pct();
+        battery_remaining = fuel.get_data;
         battery_current = battery.current_amps() * 100;
     }
 
